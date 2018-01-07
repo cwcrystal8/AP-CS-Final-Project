c5dc6031210dcb4c5a8ded066546d979268ac282
Crystal Wang <cwang4@stuy.edu>
2018-01-07 16:48:24 -0500

    Wrote data fields and method headers for all methods in RegularCandy.java

--- BeCrystaled/RegularCandy.java
+++ BeCrystaled/RegularCandy.java
@@ -1,3 +1,25 @@
 public class RegularCandy extends Candy{
+    private String type; //stores the type of RegularCandy
+
+    public RegularCandy(){
+	setIsRegular(true);
+	//Creates a random Regular Candy
+    }
+
+    public RegularCandy(int n){
+	//Creates a Regular Candy whose type is determined by n
+    }
+
+    public String getType(){
+	return "";
+    }
+
+    public int compareTo(Candy other){
+	return 0;
+    }
+
+    public boolean equals(Candy other){
+	return true;
+    }
     
 }
