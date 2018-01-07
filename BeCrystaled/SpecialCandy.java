cdb6da85709e0e61522bada3dde186a6ff63f22d
Crystal Wang <cwang4@stuy.edu>
2018-01-07 17:22:13 -0500

    Wrote mutators and accessor methods for data fields

--- BeCrystaled/SpecialCandy.java
+++ BeCrystaled/SpecialCandy.java
@@ -2,6 +2,20 @@ public abstract class SpecialCandy extends Candy{
     private int scoreWorth;
     private String type;
 
+    public void setType(String typ){
+	type = typ;
+    }
 
+    public String getType(){
+	return type;
+    }
+
+    public void setScoreWorth(int worth){
+	scoreWorth = worth;
+    }
+
+    public int getScoreWorth(){
+	return scoreWorth;
+    }
 
 }
