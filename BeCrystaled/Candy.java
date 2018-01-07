c44dd99ee776271522757fe505bf442d2fc370a0
Crystal Wang <cwang4@stuy.edu>
2018-01-07 16:18:10 -0500

    Implemented Comparable<Candy> interface and wrote necessary methods as abstract for Candy.java

--- BeCrystaled/Candy.java
+++ BeCrystaled/Candy.java
@@ -1,6 +1,10 @@
-public abstract class Candy{
+public abstract class Candy implements Comparable<Candy>{
     private boolean isRegular;
+    
     public boolean getIsRegular(){
 	return isRegular;
     }
+
+    public abstract int compareTo(Candy other);
+    public abstract boolean equals(Candy other);
 }
