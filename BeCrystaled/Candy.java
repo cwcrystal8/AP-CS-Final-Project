19c462cc2b23337e4abd6c4a8474f4300588ec75
Crystal Wang <cwang4@stuy.edu>
2018-01-07 17:10:32 -0500

    Wrote and tested methods in RegularCandy.java, added abstract getType() method in Candy.java, and added documentation comments on both

--- BeCrystaled/Candy.java
+++ BeCrystaled/Candy.java
@@ -1,14 +1,15 @@
 public abstract class Candy implements Comparable<Candy>{
-    private boolean isRegular;
+    private boolean isRegular; //stores whether the Candy is Regular or Special
     
     public boolean getIsRegular(){
-	return isRegular;
+	return isRegular; //accessor method for isRegular
     }
 
     public void setIsRegular(boolean bool){
-	isRegular = bool;
+	isRegular = bool; //mutator method for isRegular
     }
     
-    public abstract int compareTo(Candy other);
-    public abstract boolean equals(Candy other);
+    public abstract int compareTo(Candy other); //necessary for Comparable<Candy> interface
+    public abstract boolean equals(Candy other); //necessary for Comparable<Candy> interface
+    public abstract String getType(); //accessor method for data field in subclasses
 }
--- BeCrystaled/RegularCandy.java
+++ BeCrystaled/RegularCandy.java
@@ -2,24 +2,39 @@ public class RegularCandy extends Candy{
     private String type; //stores the type of RegularCandy
 
     public RegularCandy(){
-	setIsRegular(true);
-	//Creates a random Regular Candy
+	this((int)(Math.random() * 10) % 5); //Creates a random Regular Candy
     }
 
     public RegularCandy(int n){
-	//Creates a Regular Candy whose type is determined by n
+	setIsRegular(true);
+	String[] types = {"Marshmallow","JellyBean","GumDrop","JollyRancher","Skittle"};
+	type = types[n]; //Creates a Regular Candy whose type is determined by n
     }
 
     public String getType(){
-	return "";
+	return type; //accessor method for type
     }
 
     public int compareTo(Candy other){
-	return 0;
+	return 0; //unnecessary method
     }
 
     public boolean equals(Candy other){
-	return true;
+	return type == other.getType(); //returns true if the types are the same
+    }
+
+    public static void main(String[] args){
+	//Test for random constructor
+	RegularCandy a = new RegularCandy();
+	System.out.println("a: " + a.getType());
+
+	//Test for specific constructor
+	for(int i = 0; i < 5; i++){
+	    RegularCandy b = new RegularCandy(i);
+	    System.out.println("b: " + b.getType()); //Test for getType()
+	    System.out.println(a.equals(b)); //Test for equals(Candy other)
+	    System.out.println(b.equals(a)); //Test to check if reverse gives the same result, should match the output of the previous line of code
+	}
     }
     
 }
