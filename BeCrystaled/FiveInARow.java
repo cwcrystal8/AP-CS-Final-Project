8538e891ad8fc0b7227d52db583c092dd3d7c48f
Crystal Wang <cwang4@stuy.edu>
2018-01-10 15:20:22 -0500

    Wrote outline for methods

---
+++ BeCrystaled/FiveInARow.java
@@ -0,0 +1,20 @@
+public class FiveInARow extends SpecialCandy{
+    private String candyType;
+
+    public FiveInARow{
+    }
+
+    
+    private void clearAllSameType(){
+    }
+
+    public int getCandyType(){
+    }
+
+    public void setCandyType(String cantype){
+    }
+
+    public void useSpecialPower(){
+    }
+    
+}
