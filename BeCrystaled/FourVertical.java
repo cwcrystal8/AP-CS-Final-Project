3f9285e64c0dedafd550b0bf80956ac42f8e8a6c
Crystal Wang <cwang4@stuy.edu>
2018-01-09 22:00:13 -0500

    Wrote method bodies and tested them

--- BeCrystaled/FourVertical.java
+++ BeCrystaled/FourVertical.java
@@ -1,23 +1,44 @@
 public class FourVertical extends SpecialCandy{
     private int xcor;
 
-    public FourVertical{
-
+    public FourVertical(int x){
+	xcor = x;
+	setType("FourVertical");
+	setScoreWorth(300);
     }
 
 
     private void clearVertical(){
+	System.out.println("You just cleared a column!"); //Temp line, to be replaced later
     }
 
     public int getXCor(){
+	return xcor;
     }
 
     public void setXCor(int x){
-	
+	xcor = x;
     }
 
     public void useSpecialPower(){
+	clearVertical();
     }
 
+    public static void main(String[] args){
+	FourVertical a = new FourVertical(5), b = new FourVertical(6);
+	RegularCandy c = new RegularCandy();
+	
 
+	System.out.println(a.getXCor());
+	a.setXCor(8);
+	System.out.println(a.getXCor());
+	a.useSpecialPower();
+	System.out.println(a.getType());
+	System.out.println(a.getScoreWorth());
+	System.out.println(a.equals(b));
+	System.out.println(b.equals(a));
+	System.out.println(a.equals(c));
+	System.out.println(c.equals(a));
+	
+    }
 }
