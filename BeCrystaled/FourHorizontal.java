ae0706c69de921107c1284e8eb06d8816f82d1fe
Crystal Wang <cwang4@stuy.edu>
2018-01-10 15:16:42 -0500

    Wrote and tested methods

--- BeCrystaled/FourHorizontal.java
+++ BeCrystaled/FourHorizontal.java
@@ -13,16 +13,35 @@ public class FourHorizontal extends SpecialCandy{
     }
 
     public int getYCor(){
+	return ycor;
     }
 
     public void setYCor(int y){
+	ycor = y;
     }
 
     public void useSpecialPower(){
+	clearHorizontal();
     }
 
     
+    public static void main(String[] args){
+	FourHorizontal a = new FourHorizontal(5), b = new FourHorizontal(6);
+	RegularCandy c = new RegularCandy();
+	
 
+	System.out.println(a.getYCor());
+	a.setYCor(8);
+	System.out.println(a.getYCor());
+	a.useSpecialPower();
+	System.out.println(a.getType());
+	System.out.println(a.getScoreWorth());
+	System.out.println(a.equals(b));
+	System.out.println(b.equals(a));
+	System.out.println(a.equals(c));
+	System.out.println(c.equals(a));
+	
+    }
 
 
 }
