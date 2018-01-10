672a30c721d94ee6267e2dedcba025435b3b77b0
Crystal Wang <cwang4@stuy.edu>
2018-01-10 15:29:56 -0500

    Wrote  method outline

--- BeCrystaled/WrapperL.java
+++ BeCrystaled/WrapperL.java
@@ -1,6 +1,26 @@
 public class WrapperL extends SpecialCandy{
+    private int xcor, int ycor;
 
+    public WrapperL(int x, int y){
+    }
 
+    
+    private void clearSquare(){
+    }
 
+    public int getXCor(){
+    }
+
+    public int getYCor(){
+    }
+
+    public void setXCor(){
+    }
+
+    public void setYCor(){
+    }
+
+    public void useSpecialPower(){
+    }
 
 }
