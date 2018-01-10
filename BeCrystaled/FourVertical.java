156532fcfaf891e8d3826b83a2f1767f74cfc097
Crystal Wang <cwang4@stuy.edu>
2018-01-09 21:41:21 -0500

    Wrote data fields, constructors, and method headers

--- BeCrystaled/FourVertical.java
+++ BeCrystaled/FourVertical.java
@@ -1,4 +1,23 @@
 public class FourVertical extends SpecialCandy{
+    private int xcor;
+
+    public FourVertical{
+
+    }
+
+
+    private void clearVertical(){
+    }
+
+    public int getXCor(){
+    }
+
+    public void setXCor(int x){
+	
+    }
+
+    public void useSpecialPower(){
+    }
 
 
 }
