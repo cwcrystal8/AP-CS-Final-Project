18137ceef48a361ab3e2eacbd10a4ce80f78ae90
Crystal Wang <cwang4@stuy.edu>
2018-01-16 00:18:09 -0500

    Updated image for starting page

--- BeCrystaled/StartPage.java
+++ BeCrystaled/StartPage.java
@@ -6,36 +6,44 @@ import javax.swing.border.*;
 
 public class StartPage extends JFrame {
     private Container pane;
-	private JFrame home = new JFrame("Start Page");
-	private JButton start = new JButton("Start");
+    private JFrame home = new JFrame("Start Page");
+    private JButton start = new JButton("Start");
+    private JLabel backImage;
 	
-	public StartPage() {
+    public StartPage() {
 	
-		pane = this.getContentPane();
-		pane.setLayout(new BorderLayout(20,20));
-		
-		start.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				home.dispose();
-				new Window(9,9);
-			}
-		});
-		
-		JLabel intro = new JLabel("BeCrystaled\n", JLabel.CENTER);
-		intro.setFont(new Font("Times New Roman", Font.PLAIN, 60));
-		pane.add(intro, BorderLayout.NORTH);
-		
-		this.setTitle("BeCrystaled");
-		this.setSize(1000,1000);
-		this.setLocation(100,10);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		pane.add(start);
-		
-		this.setVisible(true);
-	}
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
+    
+	start.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		    home.dispose();
+		    new Window(9,9);
+		}
+	    });
+
+
+	backImage = new JLabel();
+	backImage.setLayout(new FlowLayout(FlowLayout.CENTER));
+	backImage.add(Box.createRigidArea(new Dimension(1000,475)));
+
+	backImage.setIcon(new ImageIcon("StartImage.jpg"));
+
+	start.setPreferredSize(new Dimension(100,50));
+	start.setLocation(450,525);
+	backImage.add(start);
+	pane.add(backImage);
+	this.pack();
+	this.setVisible(true);
 	
-	public static void main(String[] args) {
-		new StartPage();
-	}
+
+	this.setTitle("BeCrystaled");
+	this.setSize(1000,1000);
+	this.setLocation(100,10);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+	new StartPage();
+    }
 }
